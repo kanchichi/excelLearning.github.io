d685380b87a9e0f0ddbd58486c94384777bf8e9c
kanchichi <46985761+kanchichi@users.noreply.github.com>
2023-02-04 15:21:56 +0900

    Add files via upload

---
+++ js/newMain.js
@@ -0,0 +1,8 @@
+'use strict';{
+  const spIcon=document.getElementById('sp_menu_icon');
+  const spMenu=document.getElementById('sp_menu');
+  spIcon.addEventListener('click',()=>{
+    sp_menu.classList.toggle('disappear');
+  });
+
+}
