cb4c4dc39dda30d7f071e7691e068d71b1fdc74c
kanchichi <46985761+kanchichi@users.noreply.github.com>
2023-03-22 08:46:49 +0900

    Delete newMain.js

--- js/newMain.js
+++
@@ -1,8 +0,0 @@
-'use strict';{
-  const spIcon=document.getElementById('sp_menu_icon');
-  const spMenu=document.getElementById('sp_menu');
-  spIcon.addEventListener('click',()=>{
-    sp_menu.classList.toggle('disappear');
-  });
-
-}
